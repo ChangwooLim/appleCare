deadb846208284f7c66a63b006ead0d1e65d752f
changwoolim <limcw@zarathu.com>
2022-06-30 14:22:56 +0900

    Initial commit

---
+++ R/test.R
@@ -0,0 +1,9 @@
+#' @export
+#' @title test_print function
+#' @description print something
+#' @usage appleCare::test(1234)
+#' @param i input parameter
+
+test <- function(i = NULL){
+  print(i)
+}
